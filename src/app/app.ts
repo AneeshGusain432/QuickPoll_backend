a0cd14f98922ab067d9ab4a07a6fc7d92a693c2f
aneeshdev <anishgusain432@gmail.com>
2026-07-23 15:06:35 +0530

    add cors

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,16 +7,22 @@ import { authRouter } from "./module/auth/authRoutes.js";
 import { errorHandler } from "./common/middleware/errorMiddleware.js";
 import { pollRouter } from "./module/poll/pollRoutes.js";
 
-
 export function createExpressApplication(): Application {
   const app = express();
   app.use(express.json());
   app.use(cookieParser());
-  app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-  
+  app.use(
+    cors({
+      origin: process.env.CLIENT_URL || "http://localhost:5173",
+      credentials: true,
+    }),
+  );
+
   app.use("/api/v1/auth", authRouter);
-  app.use("/api/v1/poll", pollRouter)
-  
+  app.use("/api/v1/poll", pollRouter);
+
+  app.get("/", (req, res) => res.json({ healthy: true }));
+
   app.use(errorHandler);
   return app;
 }
