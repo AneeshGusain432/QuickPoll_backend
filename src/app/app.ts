6babc4dfcebfab02d75578c5e093a4a5a5ae4834
aneeshdev <anishgusain432@gmail.com>
2026-07-23 17:27:44 +0530

    add client url

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,7 +13,7 @@ export function createExpressApplication(): Application {
   app.use(cookieParser());
   app.use(
     cors({
-      origin: ["http://localhost:5173"],
+      origin: [process.env.CLIENT_URL ?? "http://localhost:5173"],
       credentials: true,
     }),
   );
